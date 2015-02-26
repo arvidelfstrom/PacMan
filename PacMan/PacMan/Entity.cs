4b60e68f340e3090af7aee2d8920d6f1578fac69
GoryMoon <gurreja@gmail.com>
2015-02-26 14:11:03 +0100

    Skapade grund för karaktärer

---
+++ PacMan/PacMan/Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PacMan
+{
+    public abstract class Entity
+    {
+        private String name;
+        private Vector2 coords;
+        private Vector2 speed;
+
+        public Entity(String name, Vector2 coords, Vector2 speed)
+        {
+            this.name = name;
+            this.coords = coords;
+            this.speed = speed;
+        }
+
+        public Entity(String name, float y, float x, Vector2 speed): this(name, new Vector2(x, y), speed)
+        {
+        }
+
+        public Entity(String name, float y, float x): this(name, new Vector2(x, y), new Vector2(2F, 0))
+        {
+        }
+
+        public Entity(String name, Vector2 coords): this(name, coords, new Vector2(2F, 0))
+        {
+        }
+
+        public void update(GameWindow window) {
+            this.coords += this.speed;
+        }
+
+        public void drawEntity(SpriteBatch spriteBatch) {
+            Texture2D texture = MainGame.textures[name];
+            spriteBatch.Draw(texture, coords, Color.White);
+        }
+
+        public float X { get { return coords.X; } }
+        public float Y { get { return coords.Y; } }
+        public float SpeedX { get { return speed.X; } set { speed.X = value; } }
+        public float SpeedY { get { return speed.Y; } set { speed.Y = value; } }
+        public float Width { get { return MainGame.textures[name].Width; } }
+        public float Height { get { return MainGame.textures[name].Height; } }
+        
+
+    }
+}
---
+++ PacMan/PacMan/PacMan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PacMan
+{
+    class PacMan : Entity
+    {
+
+        public PacMan(String name, float x, float y): base(name, x, y, new Vector2(0, 0))
+        {
+
+        }
+
+        public void update(GameWindow window) {
+            handleKeyboard();
+
+            base.update(window);
+        }
+
+        private void handleKeyboard() {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.A) || state.IsKeyDown(Keys.Left))
+            {
+                SpeedX = -2F;
+            }
+
+            if (state.IsKeyDown(Keys.D) || state.IsKeyDown(Keys.Right))
+            {
+                SpeedX = 2F;
+            }
+        }
+
+    }
+}
